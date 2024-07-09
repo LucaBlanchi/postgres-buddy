82a9475f62a6301c0e1f587483bfd22c6919e3ab
Luca Blanchi <lucablanchi1999@gmail.com>
2024-07-09 12:34:56 +0200

    Introduce pg-listen to react to notifications

--- src/dbClient.js
+++ src/dbClient.js
@@ -1,10 +1,30 @@
 const { Client } = require('pg');
+const createSubscriber = require('pg-listen').default;
 const dbConfig = require('../dbConfig');
+const connectionString = `postgres://${dbConfig.user}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.database}`;
 
 const client = new Client(dbConfig);
+const subscriber = createSubscriber({ connectionString });
 
 client.connect()
   .then(() => console.log('Connected to PostgreSQL'))
   .catch(err => console.error('Connection error', err.stack));
 
-module.exports = client;
+subscriber.connect().then(() => {
+  console.log("Connected to PostgreSQL for notifications");
+  subscriber.listenTo("my-channel");
+  console.log("Listening to my-channel for notifications");
+});
+
+subscriber.notifications.on("my-channel", (payload) => {
+  console.log("Received notification in 'my-channel':", payload);
+})
+
+subscriber.events.on("error", (error) => {
+  console.error("Error:", error);
+})
+
+module.exports = {
+  client,
+  subscriber,
+};
